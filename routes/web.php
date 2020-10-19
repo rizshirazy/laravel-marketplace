6e7b82778d7b1a09ec73dd782d1a4e4db130162a
Muhammad Rizki <rizkielshirazy@gmail.com>
2020-10-19 20:51:07 +0700

    apply slicing design dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardSettingController;
+use App\Http\Controllers\DashboardTransactionController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +29,16 @@ Route::get('/cart', [CartController::class, 'index'])->name('cart');
 
 Route::get('/success', [CartController::class, 'success'])->name('success');
 Route::view('/register/success', 'auth.success')->name('register-success');
+
+Route::group(['prefix' => 'dashboard'], function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dasboard');
+
+    Route::resource('/products', DashboardProductController::class)->names('dashboard.products');
+
+    Route::get('/transaction', [DashboardTransactionController::class, 'index'])->name('dashboard.transactions');
+    Route::get('/transaction/{id}', [DashboardTransactionController::class, 'show'])->name('dashboard.transactions.show');
+
+    Route::get('/settings', [DashboardSettingController::class, 'store'])->name('dashboard.settings.store');
+    Route::get('/account', [DashboardSettingController::class, 'account'])->name('dashboard.settings.account');
+});
